6c0d05eec1def83b122629d6926a4c786017082a
Kate F <follett.katie@gmail.com>
2019-02-24 04:34:32 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,9 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |name, number|
     if number == 1
-     return name
+      return name
     elsif number == 10
       return name
-
     end
   end
 end
